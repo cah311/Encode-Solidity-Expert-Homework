6ae2df764fda12625aae6481b718a99fecc3947c
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-05-03 05:51:56 -0600

    correct answer for Q1

--- Lecture6/Homework1.sol
+++ Lecture6/Homework1.sol
@@ -1,26 +1,26 @@
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
-pragma solidity >=0.7.0 <0.9.0;
-
-contract Homework {
+contract Homework6 {
     uint public balanceSentInConstructor;
-   
     // This line lets us add some Ether to this contract at deployment
-    // It has to be marked payable
     constructor() payable {
         balanceSentInConstructor = msg.value;
     }
 
-    // This function is a fallback function that lets this contract recieve Ether
+    // This function is a fallback that lets this contract recieve Ether 
     receive() payable external {}
 
-    function returnEtherReceived() public view returns(uint256) {
-        address contractAddress = address(this);
-        uint256 contractBalance;
-
+    function returnEtherReceived() public payable returns(uint256 val) {
         assembly {
-            contractBalance := balance(contractAddress)
+            val := callvalue()
         }
-        return contractBalance;
+        return val;
     }
 }
+
+
+
+
+
+
